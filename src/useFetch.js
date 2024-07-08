eb2bbe4f7fa8655fdb09369d78af127cb23a9e5a
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-07-08 20:31:40 +0330

    up to lesson 20

---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("the data could not be found on this id");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.massage);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
